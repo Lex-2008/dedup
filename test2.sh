f137938c76296dc5ac7c633658566ca726cf4e00
Aleksei Shpakovskii <aleksei.shpakovskii@cfengine.com>
2019-10-09 22:22:20 +0200

    Add scripts to filter based on first, last, and middle bytes

--- test2.sh
+++ test2.sh
@@ -1,23 +1,33 @@
 #!/bin/busybox ash
 
+# Some test
+mkdir -p test
+
+echo -n zzz>test/zzz
+echo -n yyy>test/yyy
+echo -n zyy>test/zyy
+echo -n zzy>test/zzy
+
+tree --inodes test/
+
+export MIN=0c
+export BS=1
 echo ...First, init the database
 ./init.sh
 
 echo ...Now, search for files
-./search.sh "$@"
-echo ...And filter files based on sizes
-./filter.sh
+./search.sh test
 
 echo ...Now, filter files based on first bytes
 ./first_bytes.sh
 ./filter.sh
 
-echo ...Now, filter files based on last bytes
-./last_bytes.sh
-./filter.sh
-
 echo ...Now, filter files based on middle bytes
 ./middle_bytes.sh
+./filter.sh
+
+echo ...Now, filter files based on last bytes
+./last_bytes.sh
 ./filter.sh
 
 echo ...Now, filter files based on checksums
@@ -26,3 +36,7 @@ echo ...Now, filter files based on checksums
 
 echo ...Now, hardlink similar files
 ./hardlink.sh
+
+tree --inodes test/
+
+rm -rf test
